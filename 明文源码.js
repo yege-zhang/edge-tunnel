555fcd6c2c87aafd4350a6d378aa89b3b1da3b2a
ImXiaoYe <2423336405@qq.com>
2025-02-24 21:34:06 +0800

    支持环境变量

--- 明文源码.js
+++ 明文源码.js
@@ -1,25 +1,26 @@
 import { connect } from "cloudflare:sockets"
 // 配置区块
-let 订阅路径 = "sub"
-  // 订阅路径 域名/订阅路径
-
-let 默认节点名称 = "节点"
-  // 默认节点名称
-
-let 我的UUID = "550e8400-e29b-41d4-a716-446655440000"
-  // 用于验证的UUID
+// 从环境变量读取配置，如果环境变量不存在，则使用默认值
+const 订阅路径 = env.SUB_PATH || "sub"
+      // 订阅路径 域名/订阅路径
+const 默认节点名称 = env.SUB_NAME || "节点"
+      // 默认节点名称
+const 我的UUID = env.SUB_UUID || "550e8400-e29b-41d4-a716-446655440000"
+      // 用于验证的UUID
 
 let 我的优选 = [
-  "time.is",
-  "www.shopify.com",
-  "fbi.gov"
+    "time.is",
+    "www.shopify.com",
+    "fbi.gov"
 ]
-let 我的优选TXT = [
-  //"https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/Domain.txt",
-  //"https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/HK.txt"
+const 我的优选TXT = env.TXT_URL || ""
+      //使用TXT时脚本内部填写的节点无效，二选一
+      // 格式: 地址/域名:端口#节点名称  端口不填默认443 节点名称不填则使用默认节点名称，任何都不填使用自身域名
+
+我的优选TXT = [
+//"https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/Domain.txt",
+//"https://raw.githubusercontent.com/ImLTHQ/edge-tunnel/main/HK.txt"
 ]
-    //使用TXT时脚本内部填写的节点无效，二选一
-    // 格式: 地址/域名:端口#节点名称  端口不填默认443 节点名称不填则使用默认节点名称，任何都不填使用自身域名
 
 let 启用反代功能 = true
     // 是否启用反代功能 (总开关)
@@ -27,10 +28,10 @@ let 反代地址 = "ts.hpc.tw:443"
     // 格式：地址:端口
 
 let 启用SOCKS5全局反代 = false
-let 我的SOCKS5账号 = ""
-    // 格式：账号:密码@地址:端口
+const 我的SOCKS5账号 = env.MY_SOCKS5 || ""
+      // 格式：账号:密码@地址:端口
 
-let 伪装网页 = "www.baidu.com"
+const 伪装网页 = env.FAKE_WEB || "www.baidu.com"
 
 let 启用SOCKS5反代 = false
     // 启用后原始反代将失效
